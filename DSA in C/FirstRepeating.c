d00d893537d33e8f8201b071c64c6e10340c1ede
Raghav Sharma <raghavusharmaa@gmail.com>
2026-01-23 23:24:27 +0530

    error corrected !!

--- DSA in C/FirstRepeating.c
+++ DSA in C/FirstRepeating.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
-        scanf("%d",&n);
+        scanf("%d",&arr[i]);
     }
     found = -1;
     int freq[1000000] = {0};
