aae4ddcd7a69a47297e0163beab3bdac88f9c30b
Raghav Sharma <raghavusharmaa@gmail.com>
2026-02-01 11:50:37 +0530

    File reading, analysis and most frequent word utilities

---
+++ FEB PHASE/Python/FileReader.py
@@ -0,0 +1,20 @@
+def read_file(filepath):
+    try:
+        with open(filepath, 'r') as f:# open and read
+            f_content= f.read()
+            return f_content
+                
+    except FileNotFoundError:
+        return None
+def analyze_file(filepath):
+    contents = read_file(filepath)
+    if contents is None:
+        return None
+
+    return {
+        "lines": len(contents.splitlines()),
+        "words": len(contents.split()),
+        "characters": len(contents)
+    }
+def most_frequent_word(filepath):
+    
