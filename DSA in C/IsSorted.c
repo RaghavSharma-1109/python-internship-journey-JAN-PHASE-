ea0df2579f5820951c454c4e30c21128216e1ecd
Raghav Sharma <raghavusharmaa@gmail.com>
2026-02-13 18:37:17 +0530

    DSA practice

--- DSA in C/IsSorted.c
+++ DSA in C/IsSorted.c
@@ -1,30 +1,27 @@
-# include<stdio.h>
-int isSorted(int arr[], int n){
-    if(n<2){
-        return 1;
-    }
-    for(int i=0;i<n-1;i++){
-        if(arr[i]>arr[i+1]){
-            return 0;
-        }
-    }
-    return 1;
-}
-int main(){
-    int n;
-    printf("Enter no. of elements for array:\n");
-    scanf("%d", &n);
-    int arr[n];
-    for(int i=0; i<n;i++){
-        scanf("%d" ,&arr[i]);
+#include <stdio.h>
+
+int isSorted(int arr[], int n) {
+    if (n < 2) return 1;
+
+    int i = 0;
+
+    // Skip equal elements
+    while (i < n - 1 && arr[i] == arr[i + 1]) {
+        i++;
     }
 
-    int result;
-    result = isSorted(arr,n);
-    if(result){
-        printf("The array is Sorted.\n");
-    }else{
-        printf("The array is Not Sorted.\n");
+    // If all elements are equal
+    if (i == n - 1) return 1;
+
+    // Determine direction
+    int ascending = arr[i] < arr[i + 1];
+
+    for (; i < n - 1; i++) {
+        if (ascending && arr[i] > arr[i + 1])
+            return 0;
+        if (!ascending && arr[i] < arr[i + 1])
+            return 0;
     }
-    return 0;
+
+    return ascending ? 1 : -1;
 }
--- DSA in C/Second_Largest.c
+++ DSA in C/Second_Largest.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <limits.h>
+int Second_Larget(int arr[], int n){
+    int largest = INT_MIN;
+    int second_largest = INT_MIN;
 
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] < largest && arr[i] > second_largest) {
+            second_largest = arr[i];
+        }
+    }
+    if (second_largest == INT_MIN)
+        return -1;
+    return second_largest;
+
+}
 int main() {
     int n;
     scanf("%d", &n);
@@ -14,27 +30,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    int sec_largest = Second_Larget(arr, n);
+    printf("The second Largest element in array is: %d", sec_largest);
 
-    int largest = INT_MIN;
-    int second_largest = INT_MIN;
-    int found = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > largest) {
-            second_largest = largest;
-            largest = arr[i];
-            if (second_largest != INT_MIN)
-                found = 1;
-        } else if (arr[i] < largest && arr[i] > second_largest) {
-            second_largest = arr[i];
-            found = 1;
-        }
-    }
-
-    if (!found) {
-        printf("-1");
-    } else {
-        printf("%d", second_largest);
-    }
     return 0;
 }
