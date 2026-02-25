b3683df80786d74c248fcf7f8976bfd91f88f24a
Raghav Sharma <raghavusharmaa@gmail.com>
2026-02-26 01:04:20 +0530

    Numpy Intro.

---
+++ FEB PHASE/Python/Numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+one_d = np.array([1,2,3,4,5,6])
+two_d = np.array([[1,2,3],[4,5,6]])
+
+# print(one_d)
+# print(two_d)
+# print(two_d.shape)
+
+# 1D array addition and multiplication
+print(one_d+1) # adds 1 to all element of one_d array
+print(one_d*2) # multiplies 2 to all element of one_d array
+
+# 2D array Addition and Multiplication
+print(two_d+2)
+print(two_d*4)
