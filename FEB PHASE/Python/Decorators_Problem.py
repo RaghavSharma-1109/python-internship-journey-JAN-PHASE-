1efabde84a69e4cfed3af23b569bfb63b3beaa2c
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-01 18:47:39 +0530

    Use of Decorators and Function Internals

---
+++ FEB PHASE/Python/Decorators_Problem.py
@@ -0,0 +1,37 @@
+class TypeValidationError(Exception):
+    pass
+def type_check(func):
+    def wrapper(*args,**kwargs):
+        annotations = func.__annotations__
+        para_names = func.__code__.co_varnames[:func.__code__.co_argcount]
+        values = {}
+        # Case of args
+        for name,value in zip(para_names,args):
+            values[name] = value
+        # Case of kwargs
+        for name,value in kwargs.items():
+            values[name] = value
+
+        #Type Validation
+        for name, expected_type in annotations.items():
+            if name == 'return':
+                continue
+            if name in values:
+                if not isinstance(values[name], expected_type):
+                    raise TypeValidationError(
+                        f"{name} must be of {expected_type}, got {type(values[name])}"
+                    )
+        result = func(*args,**kwargs)
+        if 'return' in annotations:
+            if not isinstance(result,annotations['return']):
+                raise TypeValidationError(
+                    f"Return must be of type {annotations['return']}, got{type(result)}"
+                    )
+        return result
+    
+    return wrapper
+@type_check
+def greet(name: str, age: int) -> str:
+    return f"{name} is {age} years old"
+
+print(greet(21, 'Raghav'))
--- FEB PHASE/Python/Numpy.py
+++ FEB PHASE/Python/Numpy.py
@@ -10,7 +10,18 @@ two_d = np.array([[1,2,3],[4,5,6]])
 # 1D array addition and multiplication
 print(one_d+1) # adds 1 to all element of one_d array
 print(one_d*2) # multiplies 2 to all element of one_d array
+# Output
+# [2 3 4 5 6 7]
+# [ 2  4  6  8 10 12]
+
 
 # 2D array Addition and Multiplication
 print(two_d+2)
 print(two_d*4)
+# OUTPUT
+# [2 3 4 5 6 7]
+# [ 2  4  6  8 10 12]
+# [[3 4 5]
+#  [6 7 8]]
+# [[ 4  8 12]
+#  [16 20 24]]
