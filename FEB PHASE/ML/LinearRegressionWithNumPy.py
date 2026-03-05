82be4889e2cc8af0db7ab3056203a8b95b75b2e8
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-05 17:12:01 +0530

    Modified With NumPy

--- FEB PHASE/ML/LinearRegressionWithNumPy.py
+++ FEB PHASE/ML/LinearRegressionWithNumPy.py
@@ -2,16 +2,16 @@ import numpy as np
 X = np.array([1,2,3,4,5])
 y = np.array([7,11,15,19,23])
 
-w = 0
+w = np.zeros(2)
 b = 0
 lr = 0.01
 epochs = 1000
 n = len(X)
 for epoch in range(epochs):
-    y_pred = w*X +b
+    y_pred = np.dot(X,w) +b
     error = y - y_pred
     loss = np.mean(error**2)
-    dw = (-2/n)*np.sum(X*error)
+    dw = (-2/n)*np.dot(X.T*error)
     db = (-2/n)*np.sum(error)
 
     w = w-lr*dw
@@ -19,6 +19,7 @@ for epoch in range(epochs):
 
     if(epoch%100==0):
         print("Epoch:", epoch, "Loss:", loss)
+
 print("Final w:", w)
 print("Final b:", b)
 print("Final loss:", loss)
