ef9dfe7b63753493d05872aa24c19ce2ad0d7f12
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-07 19:33:35 +0530

    Modified Stack Implementation

--- DSA in C/Stack_Implementation.c
+++ DSA in C/Stack_Implementation.c
@@ -8,7 +8,7 @@ void push(){
     
     if(top==SIZE-1){
         printf("Stack Overflow\n");
-        
+        return;
     }
     printf("Enter value:");
     scanf("%d", &value);
@@ -20,6 +20,7 @@ void pop(){
 
     if(top == -1){
         printf("Stack UnderFlow\n");
+        return;
     }
     printf("Removed: %d\n", stack[top]);
     top--;
@@ -27,6 +28,7 @@ void pop(){
 void peek(){
     if(top ==-1){
         printf("Stack is Empty.\n");
+        return;
     }
     printf("Top Element: %d\n", stack[top]);
 }
@@ -34,17 +36,25 @@ void display(){
 
     if(top == -1){
         printf("Stack Empty\n");
+        return;
     }
-
+    printf("Stack Elements:\n");
     for(int i = top; i >= 0; i--){
         printf("%d\n", stack[i]);
     }
 }
+int isEmpty(){
+    return top ==-1;
+}
+int isFull(){
+    return top == SIZE-1;
+}
+
 int main(){
     int choice;
 
     while(1){
-        printf("1 Push\n 2 Pop\n 3 Peek\n 4 Display\n 5 Exit\n");
+        printf("1 Push\n 2 Pop\n 3 Peek\n 4 Display\n 5 IsEmpty\n 6 IsFull\n 7 Exit\n");
         printf("Enter Choice:\n");
         scanf("%d",&choice);
         switch(choice){
@@ -64,9 +74,21 @@ int main(){
                 break;
             
             case 5:
+                if(isEmpty())
+                    printf("Stack is Empty\n");
+                else
+                    printf("Stack is Not Empty\n");
+                break;
+            case 6:
+                if(isFull())
+                    printf("Stack is Full\n");
+                else
+                    printf("Stack is Not Full\n");
+                break;
+            case 7:
                 return 0;
             default:
-                printf("Invalid Choice")
+                printf("Invalid Choice");
         }
     }
 }
