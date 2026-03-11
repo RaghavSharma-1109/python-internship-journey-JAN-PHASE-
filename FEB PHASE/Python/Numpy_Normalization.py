0e62e8ef173372491ef764c8d6f8c733235d5b9c
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-11 21:15:32 +0530

    Modified Numpy Normalization

--- FEB PHASE/Python/Numpy_Normalization.py
+++ FEB PHASE/Python/Numpy_Normalization.py
@@ -1,13 +1,20 @@
 import numpy as np
+
 A = np.random.randint(1,100,5)
 B = np.random.randint(100,1000,5)
+
 X = np.column_stack((A,B))
 
+mean_x = np.mean(X, axis=0)
+std_x = np.std(X, axis=0)
 
-mean_x = np.mean(X)
-std_x = np.std(X)
-X_norm = (X-mean_x) / std_x
+X_norm = (X - mean_x) / std_x
 
-print(f"Mean of X: {mean_x}")
-print(f"Standard Deveation of X: {std_x} ")
-print(f"Normalized X:{X_norm}")
+print("Matrix:\n", X)
+print("Mean:", mean_x)
+print("Std:", std_x)
+
+print("Normalized Matrix:\n", X_norm)
+
+print("Check Mean:", np.mean(X_norm, axis=0))
+print("Check Std:", np.std(X_norm, axis=0))
