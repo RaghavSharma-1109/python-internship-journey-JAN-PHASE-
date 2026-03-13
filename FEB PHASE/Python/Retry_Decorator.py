1499c0f932b270fe0923e730baa4fdb941fe2e6f
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-13 20:53:24 +0530

    Modified: Retry Decorator

--- FEB PHASE/Python/Retry_Decorator.py
+++ FEB PHASE/Python/Retry_Decorator.py
@@ -1,20 +1,32 @@
 import random
+from functools import wraps
+
 class MaxRetryAttemptsReached(Exception):
     pass
+
 def retry(max_attempts):
 
     def decorator(func):
 
+        @wraps(func)
         def wrapper(*args, **kwargs):
+
             for i in range(max_attempts):
                 try:
-                    return func(*args,**kwargs)
-                except Exception:
-                    if i == max_attempts-1:
-                        raise MaxRetryAttemptsReached('Try again after some time')
+                    return func(*args, **kwargs)
+
+                except Exception as e:
+                    print(f"Attempt {i+1} failed")
+
+                    if i == max_attempts - 1:
+                        raise MaxRetryAttemptsReached(
+                            "Max retry attempts reached"
+                        ) from e
+
         return wrapper
 
     return decorator
+
 
 @retry(3)
 def unreliable():
