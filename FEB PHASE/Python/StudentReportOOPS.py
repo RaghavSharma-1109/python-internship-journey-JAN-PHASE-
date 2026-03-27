fdde09ec768fc3a7140e10d4a222f0a30755aa1b
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-27 19:21:44 +0530

    Studnet Report with OOPS

--- FEB PHASE/Python/StudentReportOOPS.py
+++ FEB PHASE/Python/StudentReportOOPS.py
@@ -1,24 +1,23 @@
 class Student:
-    def __init__(self,name,marks:list) -> None:
+    def __init__(self, name, marks):
         self.name = name
         self.marks = marks
+
     def calculate_average(self):
-        sum = 0
+        total = 0
         for mark in self.marks:
-            sum += mark
-        return round(sum/len(self.marks),2)
+            total += mark
+        return round(total / len(self.marks), 2)
+
     def get_result(self):
-        if Student.calculate_average(self) > 50:
-            return 'Passed'
-        return 'Failed'
+        avg = self.calculate_average()
+        return "Passed" if avg >= 50 else "Failed"
+
     def display(self):
-        avg = Student.calculate_average(self)
-        result = Student.get_result(self)
- 
-        return( 
-            f"""
-            Name: {self.name}
-            Average: {avg}
-            Result : {result}""")
-std1 = Student('Raghav', [90,99,76])
+        avg = self.calculate_average()
+        result = self.get_result()
+        return f"Name: {self.name}\nAverage: {avg:.2f}\nResult: {result}"
+
+
+std1 = Student('Raghav', [90, 99, 76])
 print(std1.display())
